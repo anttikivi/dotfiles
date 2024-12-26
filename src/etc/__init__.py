c3aed0a1a1bc68fae3e5409c0cd3ab08284dee7a
Antti Kivi <antti@anttikivi.fi>
2024-12-26 22:59:46 +0200

    feat!: move the `etc` project to its own repository

--- src/etc/__init__.py
+++
@@ -1,5 +0,0 @@
-from etc import version
-
-__version__ = version.VERSION
-
-__all__ = ["__version__"]
--- src/etc/__main__.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-from etc.cli import main as _main
-
-if __name__ == "__main__":
-    sys.exit(_main())
