6a13ea9f77069bf53eed16dbccf528cd6d3cb87d
Antti Kivi <antti@anttikivi.fi>
2025-02-03 09:28:32 +0200

    feat: switch to Catppuccin

--- nvim/lua/anttikivi/plugins/init.lua
+++ nvim/lua/anttikivi/plugins/init.lua
@@ -3,7 +3,7 @@
 if vim.fn.has("nvim-0.10.0") == 0 then
   vim.api.nvim_echo({
     { "This Neovim configuration requires Neovim >= 0.10.0\n", "ErrorMsg" },
-    { "Press any key to exit",                                 "MoreMsg" },
+    { "Press any key to exit", "MoreMsg" },
   }, true, {})
   vim.fn.getchar()
   vim.cmd([[quit]])
