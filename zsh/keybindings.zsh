95ca6f074361cefe5c63639d4bc901150d54b8df
Antti Kivi <antti@anttikivi.fi>
2025-02-15 21:32:28 +0200

    fix: fix the search in Zsh

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -19,11 +19,9 @@ bindkey "^N" insert-last-word
 autoload -U up-line-or-beginning-search
 zle -N up-line-or-beginning-search
 
-bindkey -M viins "^[[A" up-line-or-beginning-search
-bindkey -M vicmd "^[[A" up-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
 
 autoload -U down-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey -M viins "^[[B" down-line-or-beginning-search
-bindkey -M vicmd "^[[B" down-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
