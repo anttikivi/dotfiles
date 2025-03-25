967f86d2db0977311b5be5053109734ae8492e21
Antti Kivi <antti@anttikivi.fi>
2025-03-25 18:48:51 +0200

    feat(nvim): add a directory for the plugin specs

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,11 +31,9 @@ require("config.keymaps")
 
 require("lazy").setup({
   spec = {
-    -- add your plugins here
+    { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
+  change_detection = { enabled = true, notify = false },
 })
