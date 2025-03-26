289c38722361f4b36cce372908cbc38436cf529b
Antti Kivi <antti@anttikivi.fi>
2025-03-26 23:10:21 +0200

    build: bump Neovim to 0.11

--- versions.sh
+++ versions.sh
@@ -4,7 +4,7 @@ export GO_VERSION="1.24.0"
 # Defines the release channel for Neovim to use. The possible values are:
 # "stable", "dev"
 export NVIM_CHANNEL="stable"
-export NVIM_VERSION="0.10.0"
+export NVIM_VERSION="0.11.0"
 export NVM_VERSION="0.40.1"
 export PYENV_INSTALL_VERSION="2.4.22"
 export PYTHON_VERSION="3.11"
