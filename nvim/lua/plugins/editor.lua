789abb9108b7288a77a7e912c1c5a6ef1c8023e6
Antti Kivi <antti@anttikivi.fi>
2025-03-27 19:19:38 +0200

    feat(nvim): add Harpoon

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,5 +1,59 @@
 return {
   {
+    "ThePrimeagen/harpoon",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      settings = {
+        save_on_toggle = true,
+      },
+    },
+    keys = function()
+      local keys = {
+        {
+          "<C-h>",
+          function()
+            require("harpoon"):list():add()
+          end,
+          desc = "Harpoon file",
+        },
+        {
+          "<leader>h",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Toggle Harpoon quick menu",
+        },
+      }
+
+      local ordinal = {
+        "first",
+        "second",
+        "third",
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+      }
+      for i, v in ipairs(ordinal) do
+        table.insert(keys, {
+          "<leader>" .. i,
+          function()
+            require("harpoon"):list():select(i)
+          end,
+          desc = string.format("Switch to the %s Harpoon file", v),
+        })
+      end
+
+      return keys
+    end,
+    branch = "harpoon2",
+  },
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -14,7 +68,7 @@ return {
     enabled = vim.g.finder == "telescope",
     opts = {},
     cmd = "Telescope",
-    keys = function(_, keys)
+    keys = function()
       local builtin = require("telescope.builtin")
 
       return {
