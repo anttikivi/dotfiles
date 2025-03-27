fe2209a54e2812f1d435a3f255fba418690239ec
Antti Kivi <antti@anttikivi.fi>
2025-03-27 19:54:18 +0200

    feat(nvim): add support for the `LazyFile` event

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,12 @@ require("config.options")
 require("config.autocmds")
 require("config.keymaps")
 
+local Event = require("lazy.core.handler.event")
+
+Event.mappings.LazyFile =
+  { id = "LazyFile", event = { "BufReadPost", "BufNewFile", "BufWritePre" } }
+Event.mappings["User LazyFile"] = Event.mappings.LazyFile
+
 require("lazy").setup({
   spec = {
     { import = "plugins" },
