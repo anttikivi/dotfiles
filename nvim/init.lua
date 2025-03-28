4a0b90f7fd29078edaaf17aa2161fbb3eca39720
Antti Kivi <antti@anttikivi.fi>
2025-03-28 17:25:39 +0200

    refactor(nvim): move the LazyFile setup to the event utility

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,12 +29,7 @@ require("config.options")
 require("config.autocmds")
 require("config.keymaps")
 
--- LazyFile event setup
-local Event = require("lazy.core.handler.event")
-
-Event.mappings.LazyFile =
-  { id = "LazyFile", event = { "BufReadPost", "BufNewFile", "BufWritePre" } }
-Event.mappings["User LazyFile"] = Event.mappings.LazyFile
+require("util.event").setup()
 
 -- Formatting setup
 local format = require("util.format")
--- nvim/lua/util/event.lua
+++ nvim/lua/util/event.lua
@@ -10,4 +10,12 @@ function M.on_very_lazy(fn)
   })
 end
 
+function M.setup()
+  local Event = require("lazy.core.handler.event")
+
+  Event.mappings.LazyFile =
+    { id = "LazyFile", event = { "BufReadPost", "BufNewFile", "BufWritePre" } }
+  Event.mappings["User LazyFile"] = Event.mappings.LazyFile
+end
+
 return M
