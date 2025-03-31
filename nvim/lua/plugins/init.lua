d69d0e2e5e79c9b5f59f110dd11eb9dbe02dacbb
Antti Kivi <antti@anttikivi.fi>
2025-03-31 09:28:30 +0300

    feat(nvim): add indent guides and toggle keymaps for them and inlay hints

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -3,8 +3,19 @@ return {
   {
     "folke/snacks.nvim",
     priority = 1000,
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          -- Toggle keymaps
+          Snacks.toggle.indent():map("<leader>ug")
+          Snacks.toggle.inlay_hints():map("<leader>uh")
+        end,
+      })
+    end,
     opts = {
       bigfile = { enabled = true },
+      indent = { enabled = true },
       lazygit = {
         enabled = true,
       },
