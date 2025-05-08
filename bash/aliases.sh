8b78438c1c9d810d4eb8928908067d7ea75bb385
Antti Kivi <antti@anttikivi.fi>
2025-05-08 15:48:17 +0300

    feat: add a Yazi alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -69,3 +69,12 @@ alias tls="tmux list-sessions"
 #
 # alias tf="terraform"
 # alias terrafrom="terraform"
+
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    builtin cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
+}
