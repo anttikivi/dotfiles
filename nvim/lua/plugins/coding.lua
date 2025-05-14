a2ca65f11b951b662c8bd68e1f949005b6613f92
Antti Kivi <antti@anttikivi.fi>
2025-05-14 06:13:56 +0300

    feat(nvim): add surround plugin

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -101,4 +101,18 @@ return {
       end
     end,
   },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
 }
