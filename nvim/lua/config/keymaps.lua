426fc4c55617bd33693e97cef0040204bd7714f2
Antti Kivi <antti@anttikivi.fi>
2025-06-13 16:29:11 +0300

    feat(nvim): add keymaps for moving between diagnostics

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -46,6 +46,22 @@ vim.keymap.set(
   { desc = "Line diagnostics" }
 )
 
+local diagnostic_goto = function(next, severity)
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
+end
+
+vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next diagnostic" })
+vim.keymap.set(
+  "n",
+  "[d",
+  diagnostic_goto(false),
+  { desc = "Previous diagnostic" }
+)
+
 -- Paste without overwriting the clipboard.
 vim.keymap.set(
   "x",
