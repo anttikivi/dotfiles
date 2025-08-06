4c1d160ce208351fc9d1438b1a604ea31a30fce6
Antti Kivi <antti@anttikivi.fi>
2025-08-06 20:35:21 +0300

    feat(nvim): add Harpoon and remove `lazydev` and `blink`

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,29 +8,16 @@ require("config.keymaps")
 vim.pack.add({
     colors.colorscheme_plugin_spec(),
     { src = "https://github.com/f-person/auto-dark-mode.nvim" },
-    { src = "https://github.com/folke/lazydev.nvim" },
     { src = "https://github.com/lewis6991/gitsigns.nvim.git" },
     { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-    { src = "https://github.com/Saghen/blink.cmp",                version = vim.version.range("^1.6.0") },
     { src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/ThePrimeagen/harpoon", version = "harpoon2" },
 })
 
 require("lsp").init()
 require("treesitter")
-require("blink.cmp").setup({
-    keymap = { preset = 'default' },
-    sources = {
-        default = { "lsp", "path", "snippets", "buffer", "lazydev" },
-        providers = {
-            lazydev = {
-                name = "LazyDev",
-                module = "lazydev.integrations.blink",
-                score_offset = 100,
-            },
-        },
-    },
-})
 
 if config.file_explorer == "oil" then
     require("oil").setup({
@@ -49,10 +36,17 @@ if config.file_explorer == "oil" then
     })
 end
 
-require("lazydev").setup({
-    library = {
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+local harpoon = require("harpoon")
+harpoon:setup({
+    settings = {
+        save_on_toggle = true,
     },
 })
+vim.keymap.set("n", "<C-h>", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+local ordinal = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth" }
+for i, v in ipairs(ordinal) do
+    vim.keymap.set("n", "<leader>" .. i, function() harpoon:list():select(i) end, { desc = string.format("Switch to the %s harpooned file", v) })
+end
 
 colors.init()
