7baaa489c054a86577dd15babdf22f377beb2bed
Antti Kivi <antti@anttikivi.fi>
2025-08-07 00:15:16 +0300

    feat(nvim): add Telescope and the fzf extension

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,6 +11,8 @@ vim.pack.add({
     { src = "https://github.com/lewis6991/gitsigns.nvim.git" },
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/nvim-telescope/telescope.nvim" },
+    { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
     { src = "https://github.com/stevearc/conform.nvim" },
     { src = "https://github.com/stevearc/oil.nvim" },
@@ -37,6 +39,26 @@ if config.file_explorer == "oil" then
         },
     })
 end
+
+local actions = require("telescope.actions")
+require("telescope").setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close,
+            },
+        },
+    },
+})
+if not pcall(require("telescope").load_extension, "fzf") then
+    vim.notify("Failed to load fzf extension for telescope", vim.log.levels.WARN)
+end
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
 local harpoon = require("harpoon")
 harpoon:setup({
--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -23,3 +23,60 @@ vim.api.nvim_create_autocmd("LspProgress", {
         )
     end,
 })
+
+vim.api.nvim_create_autocmd("PackChanged", {
+    group = augroup("telescope_fzf_build"),
+    callback = function(ev)
+        if ev.data.spec.name ~= "telescope-fzf-native.nvim" then
+            return
+        end
+        if ev.data.kind == "install" or ev.data.kind == "update" then
+            if vim.fn.executable("cmake") == 1 then
+                local obj = vim.system({
+                    "cmake",
+                    "-S.",
+                    "-Bbuild",
+                    "-DCMAKE_BUILD_TYPE=Release",
+                    "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
+                }, { cwd = ev.data.path }):wait()
+
+                if obj.code ~= 0 then
+                    vim.notify("Failed to build telescope-fzf-native.nvim with CMake", vim.log.levels.ERROR)
+                    vim.notify(obj.stderr, vim.log.levels.ERROR)
+
+                    return
+                end
+
+                obj = vim.system({
+                    "cmake",
+                    "--build",
+                    "build",
+                    "--config",
+                    "Release",
+                }, { cwd = ev.data.path }):wait()
+
+                if obj.code ~= 0 then
+                    vim.notify("Failed to build telescope-fzf-native.nvim with CMake", vim.log.levels.ERROR)
+                    vim.notify(obj.stderr, vim.log.levels.ERROR)
+
+                    return
+                end
+
+                vim.notify("Built telescope-fzf-native.nvim", vim.log.levels.INFO)
+            elseif vim.fn.executable("make") == 1 then
+                local obj = vim.system({ "make" }, { cwd = ev.data.path }):wait()
+
+                if obj.code ~= 0 then
+                    vim.notify("Failed to build telescope-fzf-native.nvim with make", vim.log.levels.ERROR)
+                    vim.notify(obj.stderr, vim.log.levels.ERROR)
+
+                    return
+                end
+
+                vim.notify("Built telescope-fzf-native.nvim", vim.log.levels.INFO)
+            else
+                vim.notify("Cannot built telescope-fzf-native.nvim", vim.log.levels.ERROR)
+            end
+        end
+    end,
+})
