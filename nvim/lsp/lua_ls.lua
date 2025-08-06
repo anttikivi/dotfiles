8656e14cb39e2a133247f56452e8106e1c26923d
Antti Kivi <antti@anttikivi.fi>
2025-08-06 13:17:09 +0300

    feat(nvim): add settings to the Lua language server

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -40,6 +40,24 @@ return {
         })
     end,
     settings = {
-        Lua = {},
+        Lua = {
+            codeLens = {
+                enable = true,
+            },
+            completion = {
+                callSnippet = "Replace",
+            },
+            doc = {
+                privateName = { "^_" },
+            },
+            hint = {
+                enable = true,
+                setType = false,
+                paramType = true,
+                paramName = "Disable",
+                semicolon = "Disable",
+                arrayIndex = "Disable",
+            },
+        },
     },
 }
