5b549e594b9f9ef57d7a01d8fd61c790250bf142
Antti Kivi <antti@anttikivi.fi>
2025-08-06 14:56:50 +0300

    feat(nvim): add `blink.cmp` for completions

--- nvim/init.lua
+++ nvim/init.lua
@@ -12,11 +12,25 @@ vim.pack.add({
     { src = "https://github.com/lewis6991/gitsigns.nvim.git" },
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+    { src = "https://github.com/Saghen/blink.cmp",                version = vim.version.range("^1.6.0") },
     { src = "https://github.com/stevearc/oil.nvim" },
 })
 
 require("lsp").init()
 require("treesitter")
+require("blink.cmp").setup({
+    keymap = { preset = 'default' },
+    sources = {
+        default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+        providers = {
+            lazydev = {
+                name = "LazyDev",
+                module = "lazydev.integrations.blink",
+                score_offset = 100,
+            },
+        },
+    },
+})
 
 if config.file_explorer == "oil" then
     require("oil").setup({
