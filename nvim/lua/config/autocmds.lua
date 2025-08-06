d9efe92b1ea4d4e80a2b61db8787194e81749801
Antti Kivi <antti@anttikivi.fi>
2025-08-06 07:17:37 +0300

    feat(nvim): print the LSP loading progress

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -9,3 +9,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.hl.on_yank()
     end,
 })
+
+vim.api.nvim_create_autocmd("LspProgress", {
+    group = augroup("lsp_progress"),
+    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+    callback = function(ev)
+        local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+        vim.notify(
+            -- TODO: This is not an optimal solution but kinda nice for now.
+            ev.data.params.value.kind == "end" and " "
+                or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1] .. " " .. vim.lsp.status(),
+            vim.log.levels.INFO
+        )
+    end,
+})
