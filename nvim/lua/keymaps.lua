a07cf400ebc8ac5b42552739901704e8d24ef082
Antti Kivi <antti@anttikivi.com>
2026-01-23 10:02:11 +0200

    feat(nvim): add more keymappings

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,5 +1,9 @@
 local config = require("config")
 
+-- Make up and down take line wrapping into account.
+vim.keymap.set({ "n", "o", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+vim.keymap.set({ "n", "o", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
 -- Return to file explorer.
 if config.file_explorer == "netrw" then
     vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Resume to file explorer" })
@@ -8,3 +12,19 @@ elseif config.file_explorer == "oil" then
 else
     vim.notify("[keymaps] invalid file explorer option", vim.log.levels.WARN)
 end
+
+-- Clear highlights on search and stop snippets.
+vim.keymap.set({ "i", "n", "s" }, "<esc>", function()
+    vim.cmd("noh")
+    if vim.snippet then
+        vim.snippet.stop()
+    end
+    return "<esc>"
+end, { expr = true, desc = "Clear highlights and stop snippet" })
+
+-- Better indenting.
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Delete without yanking.
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
