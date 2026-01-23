e961742e53b91867a7eae04fed0454c6f549ed59
Antti Kivi <antti@anttikivi.com>
2026-01-23 15:25:51 +0200

    refactor(nvim): move setting up the file explorer to its own module

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,22 +4,5 @@ require("options")
 require("keymaps")
 
 require("pack").setup()
-
 require("lsp").setup()
-
-if config.file_explorer == "oil" then
-    require("oil").setup({
-        default_file_explorer = true,
-        lsp_file_methods = {
-            enabled = true,
-            timeout_ms = 2000,
-        },
-        watch_for_changes = true,
-        view_options = {
-            show_hidden = true,
-            is_always_hidden = function(name)
-                return name == ".." or name == ".DS_Store"
-            end,
-        },
-    })
-end
+require("explorer").setup()
--- nvim/lua/explorer.lua
+++ nvim/lua/explorer.lua
@@ -12,4 +12,23 @@ function M.pack_specs()
     return ret
 end
 
+function M.setup()
+    if config.file_explorer == "oil" then
+        require("oil").setup({
+            default_file_explorer = true,
+            lsp_file_methods = {
+                enabled = true,
+                timeout_ms = 2000,
+            },
+            watch_for_changes = true,
+            view_options = {
+                show_hidden = true,
+                is_always_hidden = function(name)
+                    return name == ".." or name == ".DS_Store"
+                end,
+            },
+        })
+    end
+end
+
 return M
