fe7c7198ad80e60ecb06df89c3cfe48f97fba810
Antti Kivi <antti@anttikivi.com>
2026-01-23 15:57:06 +0200

    feat(nvim): add utility for memoizing function results

---
+++ nvim/lua/dot/util.lua
@@ -0,0 +1,22 @@
+---@class dot.util
+local M = {}
+
+local cache = {} ---@type table<(fun()), table<string, any>>
+
+---@generic T: fun()
+---@param fn T
+---@return T
+function M.memoize(fn)
+    return function(...)
+        local key = vim.inspect({ ... })
+        cache[fn] = cache[fn] or {}
+
+        if cache[fn][key] == nil then
+            cache[fn][key] = fn(...)
+        end
+
+        return cache[fn][key]
+    end
+end
+
+return M
