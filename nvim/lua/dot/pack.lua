26cc0f3a4e64922f9ff2a802076e5b729994f461
Antti Kivi <antti@anttikivi.com>
2026-01-24 08:41:28 +0200

    feat(nvim): add color scheme

--- nvim/lua/dot/pack.lua
+++ nvim/lua/dot/pack.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.setup()
     local pack_specs = {}
 
+    vim.list_extend(pack_specs, require("dot.colors").pack_specs())
     vim.list_extend(pack_specs, require("dot.explorer").pack_specs())
     vim.list_extend(pack_specs, require("dot.lsp").pack_specs())
     vim.list_extend(pack_specs, require("dot.mason").pack_specs())
