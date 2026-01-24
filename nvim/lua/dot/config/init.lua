9c01609d1e53f9abba5de87c96b81dc1a481d190
Antti Kivi <antti@anttikivi.com>
2026-01-24 09:01:45 +0200

    feat(nvim): add diagnostics config

--- nvim/lua/dot/config/init.lua
+++ nvim/lua/dot/config/init.lua
@@ -1,7 +1,63 @@
 ---@class dot.Config
 local M = {
+    ---@type boolean
+    enable_icons = false,
+
     ---@type "netrw" | "oil"
     file_explorer = "oil",
+    icons = {
+        diagnostics = {
+            error = "󰅚 ",
+            warn = "󰀪 ",
+            info = "󰋽 ",
+            hint = "󰌶 ",
+        },
+        kinds = {
+            Array = " ",
+            Boolean = "󰨙 ",
+            Class = " ",
+            Codeium = "󰘦 ",
+            Color = " ",
+            Control = " ",
+            Collapsed = " ",
+            Constant = "󰏿 ",
+            Constructor = " ",
+            Copilot = " ",
+            Enum = " ",
+            EnumMember = " ",
+            Event = " ",
+            Field = " ",
+            File = " ",
+            Folder = " ",
+            Function = "󰊕 ",
+            Interface = " ",
+            Key = " ",
+            Keyword = " ",
+            Method = "󰊕 ",
+            Module = " ",
+            Namespace = "󰦮 ",
+            Null = " ",
+            Number = "󰎠 ",
+            Object = " ",
+            Operator = " ",
+            Package = " ",
+            Property = " ",
+            Reference = " ",
+            Snippet = "󱄽 ",
+            String = " ",
+            Struct = "󰆼 ",
+            Supermaven = " ",
+            TabNine = "󰏚 ",
+            Text = " ",
+            TypeParameter = " ",
+            Unit = " ",
+            Value = " ",
+            Variable = "󰀫 ",
+        },
+        statusline = {
+            branch = " ",
+        },
+    },
 }
 
 return M
--- nvim/lua/dot/lsp.lua
+++ nvim/lua/dot/lsp.lua
@@ -1,3 +1,4 @@
+local config = require("dot.config")
 local util = require("dot.util")
 
 local M = {}
@@ -46,6 +47,23 @@ function M.setup()
             vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
         end
     end)
+
+    vim.diagnostic.config({
+        signs = {
+            text = config.enable_icons and {
+                [vim.diagnostic.severity.ERROR] = config.icons.diagnostics.error,
+                [vim.diagnostic.severity.WARN] = config.icons.diagnostics.warn,
+                [vim.diagnostic.severity.INFO] = config.icons.diagnostics.info,
+                [vim.diagnostic.severity.HINT] = config.icons.diagnostics.hint,
+            } or {
+                [vim.diagnostic.severity.ERROR] = "E",
+                [vim.diagnostic.severity.WARN] = "W",
+                [vim.diagnostic.severity.INFO] = "I",
+                [vim.diagnostic.severity.HINT] = "H",
+            },
+        },
+        virtual_text = true,
+    })
 
     require("lazydev").setup({
         library = {
