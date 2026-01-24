338bd24bc05a0d6273dc3b553ba524fe6c5889af
Antti Kivi <antti@anttikivi.com>
2026-01-24 08:36:02 +0200

    feat(nvim): add the LSP keymaps and enable inlay hints

--- nvim/lua/dot/lsp.lua
+++ nvim/lua/dot/lsp.lua
@@ -7,12 +7,45 @@ local M = {}
 ---@type table<string, dot.lsp.Config>
 local servers = {}
 
+-- Register a function to be run with an autocommand when a language server attaches to a buffer.
+---@param fn fun(client: vim.lsp.Client, buf: integer)
+---@param name? string
+local function on_attach(fn, name)
+    return vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+            local buffer = args.buf ---@type integer
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            if client and (not name or client.name == name) then
+                fn(client, buffer)
+            end
+        end,
+    })
+end
+
 function M.setup()
     for name, server in pairs(servers) do
         vim.lsp.config(name, server)
     end
 
     vim.lsp.enable(M.get_server_names())
+
+    on_attach(function(_, buffer)
+        -- Neovim now provides some default mappings so I don't need to add my
+        -- own:
+        -- "grn": vim.lsp.buf.rename()
+        -- "gra": vim.lsp.buf.code_action()
+        -- "grr": vim.lsp.buf.references()
+        -- "gri": vim.lsp.buf.implementation()
+        -- "grt": vim.lsp.buf.type_definition()
+        -- "gO": vim.lsp.buf.document_symbol()
+        -- CTRL-S: vim.lsp.buf.signature_help()
+        vim.keymap.set("n", "grd", vim.lsp.buf.definition, { buffer = buffer })
+    end)
+    on_attach(function(client, buffer)
+        if client:supports_method("textDocument/inlayHint") then
+            vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
+        end
+    end)
 
     require("lazydev").setup({
         library = {
