e13a4e41b47fad95e37d6ef1918e9c224ab012d5
Antti Kivi <antti@anttikivi.com>
2026-01-25 10:54:38 +0200

    refactor(nvim): move diagnostics options into their own file

--- nvim/lua/dot/lsp.lua
+++ nvim/lua/dot/lsp.lua
@@ -1,4 +1,3 @@
-local config = require("dot.config")
 local util = require("dot.util")
 
 local M = {}
@@ -35,23 +34,6 @@ function M.setup()
             vim.lsp.inlay_hint.enable(true, { bufnr = buffer })
         end
     end)
-
-    vim.diagnostic.config({
-        signs = {
-            text = config.enable_icons and {
-                [vim.diagnostic.severity.ERROR] = config.icons.diagnostics.error,
-                [vim.diagnostic.severity.WARN] = config.icons.diagnostics.warn,
-                [vim.diagnostic.severity.INFO] = config.icons.diagnostics.info,
-                [vim.diagnostic.severity.HINT] = config.icons.diagnostics.hint,
-            } or {
-                [vim.diagnostic.severity.ERROR] = "E",
-                [vim.diagnostic.severity.WARN] = "W",
-                [vim.diagnostic.severity.INFO] = "I",
-                [vim.diagnostic.severity.HINT] = "H",
-            },
-        },
-        virtual_text = true,
-    })
 
     require("lazydev").setup({
         library = {
