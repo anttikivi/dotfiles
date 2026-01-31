e246591e7ab84c7eeffa05562bc3153f6347c334
Antti Kivi <antti@anttikivi.com>
2026-01-31 10:38:39 +0200

    feat(zsh): add more keybindings

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,3 +1,17 @@
+bindkey -e
+
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
+
+bindkey "^A" beginning-of-line
+bindkey "^E" end-of-line
+bindkey "^K" kill-line
+bindkey "^R" history-incremental-search-backward
+bindkey "^P" history-search-backward
+bindkey "^Y" accept-and-hold
+bindkey "^N" insert-last-word
+
 autoload -U up-line-or-beginning-search
 zle -N up-line-or-beginning-search
 
