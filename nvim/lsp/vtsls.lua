c7543c8b8e0ffdb758401df6247c12ca299752a2
Antti Kivi <antti@anttikivi.com>
2026-02-24 11:08:20 +0200

    feat(nvim): switch to a different TypeScript language server

--- nvim/lsp/vtsls.lua
+++ nvim/lsp/vtsls.lua
@@ -1,18 +1,16 @@
 ---@type vim.lsp.Config
 return {
-    cmd = function(dispatchers, config)
-        local cmd = "tsgo"
-        local local_cmd = (config or {}).root_dir and config.root_dir .. "/node_modules/.bin/tsgo"
-        if local_cmd and vim.fn.executable(local_cmd) == 1 then
-            cmd = local_cmd
-        end
-        return vim.lsp.rpc.start({ cmd, "--lsp", "--stdio" }, dispatchers)
-    end,
+    cmd = { "vtsls", "--stdio" },
+    init_options = {
+        hostInfo = "neovim",
+    },
     filetypes = {
         "javascript",
         "javascriptreact",
+        "javascript.jsx",
         "typescript",
         "typescriptreact",
+        "typescript.tsx",
     },
     root_dir = function(bufnr, on_dir)
         -- The project root is where the LSP can be started from
@@ -23,7 +21,7 @@ return {
         -- Give the root markers equal priority by wrapping them in a table
         root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers, { ".git" } }
             or vim.list_extend(root_markers, { ".git" })
-
+        -- exclude deno
         local deno_root = vim.fs.root(bufnr, { "deno.json", "deno.jsonc" })
         local deno_lock_root = vim.fs.root(bufnr, { "deno.lock" })
         local project_root = vim.fs.root(bufnr, root_markers)
