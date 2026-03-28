11c3d82c8e7d32aa8c5841478f5307af41520a8c
Antti Kivi <antti@anttikivi.com>
2026-03-28 10:58:10 +0200

    fix: fix the ZLS installation

--- nvim/lsp/zls.lua
+++ nvim/lsp/zls.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { vim.fn.expand("~/.local/bin/zls") },
+    cmd = { vim.fn.expand("~/.local/opt/zls/bin/zls") },
     filetypes = { "zig", "zir" },
     root_markers = { "zls.json", "build.zig", ".git" },
     workspace_required = false,
